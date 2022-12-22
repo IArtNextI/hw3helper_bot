cd0b6d6143a98473727b06ce4bed6baf9a953455
Artem Khromov <artem.a.khromov@gmail.com>
2022-12-23 00:48:07 +0300

    Updates

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t Dockerfile.base -f hw3helper_bot_base:latest .
-docker build -t Dockerfile.bot -f hw3helper_bot_bot:latest .
-docker build -t Dockerfile.db -f hw3helper_bot_db:latest .
+docker build -f Dockerfile.base -t hw3helper_bot_base:latest .
+docker build -f Dockerfile.bot -t hw3helper_bot_bot:latest .
+docker build -f Dockerfile.db -t hw3helper_bot_db:latest .
