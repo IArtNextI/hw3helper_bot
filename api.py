c1fa3c1d774d39248fc56412503c64409d1df0f7
Artem Khromov <artem.a.khromov@gmail.com>
2022-12-23 00:15:38 +0300

    API and bot updates

---
+++ api.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+from pickle import load, dump
+from os import mkdir, path
+
+app = Flask(__name__)
+
+@app.route("/api/<username>/<element>", methods=["GET", "POST"])
+def resolve(username, element):
+    if request.method == "GET":
+        if not path.isdir(f"storage/{username}/"):
+            mkdir(f"storage/{username}/")
+        with open(f"storage/{username}/{element}", "rb") as fin:
+            return load(fin)
+    else:
+        if not path.isdir(f"storage/{username}/"):
+            mkdir(f"storage/{username}/")
+        with open(f"storage/{username}/{element}", 'wb') as fout:
+            dump(request.get_data(), fout)
+            return "OK"
--- bot.py
+++ bot.py
@@ -3,22 +3,60 @@
 import telebot.async_telebot
 import os
 import asyncio
+import requests
 
 bot = telebot.async_telebot.AsyncTeleBot(os.environ["TOKEN"])
+storage_node = os.environ["STORAGE_NODE"]
 
 @bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
-    await bot.reply_to(message, "Hello!")
+async def start(message):
+    await bot.reply_to(message, "/help - Displays this message\n\
+/getid - Displays user's id\n\
+/chatid - Displays chat id\n\
+/calc - Allows to evaluate various mathematical expression\n\
+/set - Allows to store data\n\
+/get - Allows to retrieve stored data\n")
 
 @bot.message_handler(commands=['getid'])
-async def send_welcome(message):
+async def getid(message):
     await bot.reply_to(message, message.from_user.id)
 
-@bot.message_handler(func=lambda x: True)
-async def send_welcome(message):
-    print(message.from_user)
-    print(message.text)
-    await bot.reply_to(message, input())
+@bot.message_handler(commands=['chatid'])
+async def chatid(message):
+    await bot.reply_to(message, message.from_user.id)
 
+@bot.message_handler(commands=['calc'])
+async def calc(message):
+    expression = message.text[6:].strip()
+    for x in expression:
+        if x not in "0123456789 *+-/^&|()" and not x.isalpha():
+            await bot.reply_to(message, "Found forbidden symbol '" + x + "'")
+            return 
+    try:
+        result = eval(expression)
+        await bot.reply_to(message, str(result))
+    except:
+        await bot.reply_to(message, "Failed to evaluate the expression")
+
+@bot.message_handler(commands=['set'])
+async def set_(message):
+    expression = message.text[5:].strip()
+    name, value = expression.split()
+    for x in name:
+        if not x.isalpha():
+            await bot.reply_to(message, "Found forbidden symbol '" + x + "' in name")
+            return
+    response = requests.post(storage_node + "/api/" + str(message.from_user.id) + "/" + name, data=value.encode())
+    await bot.reply_to(message, ("Stored" if response.text == "OK" else "Failed to store"))
+
+@bot.message_handler(commands=['get'])
+async def set_(message):
+    name = message.text[5:].strip()
+    for x in name:
+        if not x.isalpha():
+            await bot.reply_to(message, "Found forbidden symbol '" + x + "' in name")
+            return
+    response = requests.get(storage_node + "/api/" + str(message.from_user.id) + "/" + name)
+    await bot.reply_to(message, "The recovered value is '" + response.text + "'")
 
 asyncio.run(bot.polling())
